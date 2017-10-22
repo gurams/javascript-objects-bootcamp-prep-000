82e2d8b58ab22b4cc1128c96d5f7a2c50bfd1ef2
Ramesh <gurams@gmail.com>
2017-10-22 01:57:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var   playlist = {artName : "Song title" , artName2 : "Song title2"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return playlist={artistName : songTitle}
+  return playlist={[artistName] : songTitle}
 }
