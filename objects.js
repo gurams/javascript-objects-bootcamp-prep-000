4fbae8a3d092d86fdf34bd501b88d6d3d98737b2
Ramesh <gurams@gmail.com>
2017-10-22 01:58:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var   playlist = {artName : "Song title" , artName2 : "Song title2"};
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist={[artistName] : songTitle}
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+}
